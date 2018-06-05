501f4a1a8c7aae20d665d10b041816d576696bb8
raul <raul@DESKTOP-QS3PBVH>
2018-06-05 18:25:48 +0200

    Segundo commit

--- Swing - Texto/src/Main.java
+++ Swing - Texto/src/Main.java
@@ -24,7 +24,7 @@ public class Main extends JFrame{
 
 	
 	public Main() {
-		super("Líneas");
+		super("Lineas texto");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container c = getContentPane();
 		// crear y añadir el Lienzo
